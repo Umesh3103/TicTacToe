d2599732e4f181c6968ab7428a5a91f4b84e4760
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-07 09:56:12 +0530

    [Umesh]Added: creating board

--- src/com/bl/tictactoe/TicTacToeGame.java
+++ src/com/bl/tictactoe/TicTacToeGame.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("welcome to tictactoe problem");
+		char[] board = createBoard();
+	}
+
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for(int position=1;position<10;position++){
+			board[position] =' ';
+		}
+		return board;
 	}
 }
+
