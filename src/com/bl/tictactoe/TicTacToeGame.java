a4df3a66f418e223ee51771ff5a58189f420ed04
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-07 11:11:20 +0530

    [Umesh]Refactored: printing the board

--- src/com/bl/tictactoe/TicTacToeGame.java
+++ src/com/bl/tictactoe/TicTacToeGame.java
@@ -5,45 +5,30 @@ import java.util.Scanner;
 public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("welcome to tictactoe problem");
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		char[] board = createBoard();
-		char option = chooseOption(sc);
-		if(option == 'X' || option == 'x'){
-			System.out.println("Player symbol");
-		}
-		else if(option == 'O' || option == 'o'){
-			System.out.println("Computer symbol");
-		}
-		else{
-			System.out.println("Enter proper symbol");
-		}
+		char option = (chooseOption(sc) == 'X') ? 'O' : 'X';
 		showBoard(board);
-		
 	}
+
 	private static void showBoard(char[] board) {
 		// TODO Auto-generated method stub
-		for(int position = 1; position < 4; position++){
-			for(int row = 1; row < 4; row++){
-				System.out.print("  |"+board[position]);
-			}
-			System.out.println();
-			System.out.print("-------------");
-			System.out.println();
-		}
-		
+		System.out.println(board[1]+"  |"+board[2]+"  |"+board[3]+"\n"+"-----------"+"\n"+board[4]+"  |"+board[5]+"  |"+board[6]+"\n"
+				+"-----------"+"\n"+board[7]+"  |"+board[8]+"  |"+board[9]);
 	}
+
 	// creating the board
 	private static char[] createBoard() {
 		char[] board = new char[10];
-		for(int position=1;position<10;position++){
-			board[position] =' ';
+		for (int position = 1; position < 10; position++) {
+			board[position] = ' ';
 		}
 		return board;
 	}
+
 	// taking an input character from user and returning to the main
-	private static char  chooseOption(Scanner sc){
+	private static char chooseOption(Scanner sc) {
 		System.out.println("Enter the symbol");
 		return sc.next().charAt(0);
 	}
 }
-
