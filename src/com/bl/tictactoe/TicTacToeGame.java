4543bef0549c35470450fd7ea588641af048c38e
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-07 10:08:54 +0530

    [Umesh]Added: taking input from user for assigning to computer and player

--- src/com/bl/tictactoe/TicTacToeGame.java
+++ src/com/bl/tictactoe/TicTacToeGame.java
@@ -6,14 +6,31 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("welcome to tictactoe problem");
 		char[] board = createBoard();
+		char option = chooseOption();
+		if(option == 'X' || option == 'x'){
+			System.out.println("Player symbol");
+		}
+		else if(option == 'O' || option == 'o'){
+			System.out.println("Computer symbol");
+		}
+		else{
+			System.out.println("Enter proper symbol");
+		}
 	}
-
+	// creating the board
 	private static char[] createBoard() {
 		char[] board = new char[10];
 		for(int position=1;position<10;position++){
 			board[position] =' ';
 		}
 		return board;
+	}
+	// taking an input character from user and returning to the main
+	private static char chooseOption(){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the symbol");
+		char symbol= sc.next().charAt(0);
+		return symbol;
 	}
 }
 
