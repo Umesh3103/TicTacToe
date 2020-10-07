1638f44ae75c2a72c139dc0bbffff9410ed65265
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-10-07 21:22:15 +0530

    [Umesh]Added: checking for winning move

--- src/com/bl/tictactoe/TicTacToeGame.java
+++ src/com/bl/tictactoe/TicTacToeGame.java
@@ -3,15 +3,167 @@ package com.bl.tictactoe;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("welcome to tictactoe problem");
 		Scanner sc = new Scanner(System.in);
 		char[] board = createBoard();
 		char toss =whoStartFirst();
-		enterValue(board, sc, toss);
+		twoPlayers(board,sc,toss);
 	}
 	
+	private static void twoPlayers(char[] board,Scanner sc,char turn) {
+		while(true){
+			enterValue(board,sc,turn);
+			int check = checkWinner(board,turn);
+			if(check==1){
+				break;
+			}
+			if(turn == 'X'){
+				turn ='O';
+			}
+			else{
+				turn = 'X';
+			}
+			int result = checkWinningMove(board,turn);
+			if(result ==1){
+				break;
+			}
+		}
+		showBoard(board);
+		checkWinner(board,turn);
+	}
+
+	
+	private static int checkWinningMove(char[] board, char turn) {
+		int flag=0;
+		if(board[1]==turn&& board[2]==turn){
+			if(board[3]==' '){
+				board[3]=turn;
+				flag=1;
+			}
+		}
+		if(board[2]==turn&& board[3]==turn){
+			if(board[1]==' '){
+				board[1]=turn;
+				flag=1;
+			}
+		}if(board[1]==turn&& board[3]==turn){
+			if(board[2]==' '){
+				board[2]=turn;
+				flag=1;
+			}
+		}if(board[4]==turn&& board[5]==turn){
+			if(board[6]==' '){
+				board[6]=turn;
+				flag=1;
+			}
+		}if(board[4]==turn&& board[6]==turn){
+			if(board[5]==' '){
+				board[5]=turn;
+				flag=1;
+			}
+		}if(board[5]==turn&& board[6]==turn){
+			if(board[4]==' '){
+				board[4]=turn;
+				flag=1;
+			}
+		}if(board[7]==turn&& board[8]==turn){
+			if(board[9]==' '){
+				board[9]=turn;
+				flag=1;
+			}
+		}if(board[7]==turn&& board[9]==turn){
+			if(board[8]==' '){
+				board[8]=turn;
+				flag=1;
+			}
+		}if(board[8]==turn&& board[9]==turn){
+			if(board[7]==' '){
+				board[7]=turn;
+				flag=1;
+			}
+		}if(board[1]==turn&& board[4]==turn){
+			if(board[7]==' '){
+				board[7]=turn;
+				flag=1;
+			}
+		}if(board[1]==turn&& board[7]==turn){
+			if(board[4]==' '){
+				board[4]=turn;
+				flag=1;
+			}
+		}if(board[4]==turn&& board[7]==turn){
+			if(board[1]==' '){
+				board[1]=turn;
+				flag=1;
+			}
+		}if(board[2]==turn&& board[5]==turn){
+			if(board[8]==' '){
+				board[8]=turn;
+				flag=1;
+			}
+		}if(board[2]==turn&& board[8]==turn){
+			if(board[5]==' '){
+				board[5]=turn;
+				flag=1;
+			}
+		}if(board[5]==turn&& board[8]==turn){
+			if(board[2]==' '){
+				board[2]=turn;
+				flag=1;
+			}
+		}if(board[3]==turn&& board[6]==turn){
+			if(board[9]==' '){
+				board[9]=turn;
+				flag=1;
+			}
+		}if(board[3]==turn&& board[9]==turn){
+			if(board[6]==' '){
+				board[6]=turn;
+				flag=1;
+			}
+		}if(board[6]==turn&& board[9]==turn){
+			if(board[3]==' '){
+				board[3]=turn;
+				flag=1;
+			}
+		}if(board[1]==turn&& board[5]==turn){
+			if(board[9]==' '){
+				board[9]=turn;
+				flag=1;
+			}
+		}if(board[1]==turn&& board[9]==turn){
+			if(board[5]==' '){
+				board[5]=turn;
+				flag=1;
+			}
+		}if(board[5]==turn&& board[9]==turn){
+			if(board[1]==' '){
+				board[1]=turn;
+				flag=1;
+			}
+		}if(board[3]==turn&& board[5]==turn){
+			if(board[7]==' '){
+				board[7]=turn;
+				flag=1;
+			}
+		}if(board[3]==turn&& board[7]==turn){
+			if(board[5]==' '){
+				board[5]=turn;
+				flag=1;
+			}
+		}if(board[5]==turn&& board[7]==turn){
+			if(board[3]==' '){
+				board[3]=turn;
+				flag=1;
+			}
+		}
+		return flag;
+		
+	}
+
 	private static int checkWinner(char[] board, char toss) {
 		if(isWinning(board,toss)){
 			System.out.println("Winner is "+toss);
@@ -46,26 +198,18 @@ public class TicTacToeGame {
 	// entering value and printing the board
 	private static void enterValue(char[] board, Scanner sc, char option) {
 		int userInput,check;
-		while (true) {
 			System.out.println("Enter the index you wants to put the value");
 			userInput = sc.nextInt();
 			if (userInput >= 1 && userInput < 10) {
 				if (board[userInput] != ' ') {
 					System.out.println("the index is not free");
-					break;
 				} else {
 					board[userInput] = option;
 					showBoard(board);
-					check = checkWinner(board,option);
-					if(check==1){
-						option = (option == 'X')? 'O' : 'X';
-						break;
-					}
 				}
 			} else {
 				System.out.println("Enter proper index");
 			}
-		}
 	}
 
 	
